4d6ab5f37014642ef5a09aff42c2c0da79a1ab7b
davidgames3d <pmartinsd9@gmail.com>
2025-09-16 12:25:08 -0300

    cancelado

--- DARKHUB🟣.lua
+++ DARKHUB🟣.lua
@@ -1,15 +1,4 @@
-local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/Library-ui/refs/heads/main/Redzhubui"))()
-local Window = redzlib:MakeWindow({
-  Title = "dark hub💜 : Universal",
-  SubTitle = "by davidgames3d",
-  SaveFolder = "testando | redz lib v5.lua"
-})
-local Tab1 = Window:MakeTab({"main", "main"})
-
-  redzlib:SetTheme("Purple")
-
-Window:AddMinimizeButton({
-    Button = { Image = "rbxassetid://114026258621824", BackgroundTransparency = 0 },
+", BackgroundTransparency = 0 },
     Corner = { CornerRadius = UDim.new(35, 1) },
 })
 
