9dc43bbfab6654d058361ab0a9afd7ff768c3e6d
davidgames3d <pmartinsd9@gmail.com>
2025-09-16 12:22:14 -0300

    Atualizar o DARKHUB🟣.lua

--- DARKHUB🟣.lua
+++ DARKHUB🟣.lua
@@ -1,6 +1,6 @@
 local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/Library-ui/refs/heads/main/Redzhubui"))()
 local Window = redzlib:MakeWindow({
-  Title = "dark hub : Universal",
+  Title = "dark hub💜 : Universal",
   SubTitle = "by davidgames3d",
   SaveFolder = "testando | redz lib v5.lua"
 })
