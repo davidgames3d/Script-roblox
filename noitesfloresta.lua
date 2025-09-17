38b3cf63abb66affbdad95f784a182677b744369
davidgames3d <pmartinsd9@gmail.com>
2025-09-17 15:52:04 -0300

    Criar noitesfloresta.lua

---
+++ noitesfloresta.lua
@@ -0,0 +1,21 @@
+local Tab1 = Window:MakeTab({"99 noites na floresta🌲", "99 noites na floresta🌲"})
+
+Tab1:AddButton({"darkESC hub", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DarkenedEssence/DarkEsc/refs/heads/main/Loader.lua"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"SkyBase Hub🏗", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nouralddin-abdullah/99-night/refs/heads/main/main-en.lua"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"The DarKones", function(Value)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/TheDarkoneMarcillisePex/Other-Scripts/refs/heads/main/99%20Nights%20In%20The%20Forest%20GUI'))()
+print("Hello World!")
+end}) 
+
+Tab1:AddButton({"auto Farm and more", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/99-Nights-in-the-Forest-STRONGHOLDS-KEYLESS-auto-farm-bring-item-teleports-and-more-by-Moondiety-44416"))()
+print("Hello World!")
+end})
