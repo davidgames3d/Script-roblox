778e3cf767b332f0dd85c843560a6798440305de
davidgames3d <pmartinsd9@gmail.com>
2025-09-17 15:53:44 -0300

    Criar glowagarden.lua

---
+++ glowagarden.lua
@@ -0,0 +1,36 @@
+local Tab1 = Window:MakeTab({"glow a Garden🌻", "glow a Garden🌻"})
+
+Tab1:AddButton({"auto Farm gui", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Grow-a-Garden-GAG-Auto-Farm-GUI-Keyless-41847"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"ffj1", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FFJ1/Roblox-Exploits/main/scripts/Loader.lua"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"glow a garden", function(Value)
+loadstring(game:HttpGet("https://pastebin.com/raw/A7us0FyY", true))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"jstarzz", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jstarzz/petmover/refs/heads/main/main.lua", true))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"glow a Garden auto", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Grow-a-Garden-Best-Script-working-auto-53253"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"rift hub glow a garden", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Grow-a-Garden-Freeze-All-Pets-43750"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"infinite money dupe", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Grow-a-Garden-KEYLESS-Grow-a-garden-INFINITE-MONEY-DUPE-auto-farm-script-41396"))()
+print("Hello World!")
+end})
