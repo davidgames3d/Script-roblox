20e1d25be296cced3c3aa02c5eded5852cfff5ac
davidgames3d <pmartinsd9@gmail.com>
2025-09-17 16:01:23 -0300

    Criar auto.lua

---
+++ auto.lua
@@ -0,0 +1,51 @@
+local Tab1 = Window:MakeTab({"AUTO FARM,AIMBOT,SPY", "AUTO FARM,AIMBOT,SPY"})
+
+Tab1:AddButton({"OP Infinite Stamina Auto Hunt Players Auto Farm", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OhhMyGehlee/sak/refs/heads/main/for"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"OP SCRIPT KILL AURA AUTO RAID AUTO DUNGEON AND MORE", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OhhMyGehlee/sh/refs/heads/main/a"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"OP Fast Kill Aura Auto Hatch Auto Roll Auto Farm", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OhhMyGehlee/sh/refs/heads/main/a"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"battleground", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hehehe9028/THE-strongest-battleground/refs/heads/main/HOKALAZA%20The%20strongest%20battleground"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"The Strongest Battlegrounds", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hehehe9028/THE-strongest-battleground/refs/heads/main/HOKALAZA%20The%20strongest%20battleground"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"Fish Key Lessa script", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KoonpeatchRE/Lonely_Hub/refs/heads/main/Fish_It"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"esp and hitbox expander", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EnesKam21/deadlineesp/refs/heads/main/deadlineesp.lua"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"rainbow script", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ArceusXArchivezx/Game/refs/heads/main/ArceusXArchive"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"knockout simulator", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Matej1912/Knockout-Simulator/refs/heads/main/Script"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"insta kill", function(Value)
+loadstring(game:HttpGet("https://noxhub.store/loader"))()
+print("Hello World!")
+end})
