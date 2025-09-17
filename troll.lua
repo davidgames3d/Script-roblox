c47a52e8435e79b22b768b1e50c599914c3c2f6c
davidgames3d <pmartinsd9@gmail.com>
2025-09-17 16:02:31 -0300

    Criar troll.lua

---
+++ troll.lua
@@ -0,0 +1,27 @@
+local Tab1 = Window:MakeTab({"trollagem🤣", "trollagem🤣"})
+
+
+Tab1:AddButton({"music player gui", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/davidgames3d/Script-roblox/main/Music%20playey.lua"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"kater hub universal", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KATERGaming/Roblox/main/KaterHub.Lua"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"FE Trolling GUI", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yofriendfromschool1/Sky-Hub/main/FE%20Trolling%20GUI.luau"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"soluna troll brookhaven", function(Value)
+loadstring(game:HttpGet("https://soluna-script.vercel.app/brookhaven.lua",true))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"conquista fake doors", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/davidgames3d/Script-roblox/main/conquista%20falsa%20doors.lua"))()
+print("Hello World!")
+end})
