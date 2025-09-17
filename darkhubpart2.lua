33d75cb81dcb0c9a0402df3a10c529793d3d949b
davidgames3d <pmartinsd9@gmail.com>
2025-09-17 16:30:40 -0300

    Atualizar o darkhubpart2.lua

--- darkhubpart2.lua
+++ darkhubpart2.lua
@@ -30,4 +30,83 @@ Tab1:AddButton({"ir para parte 3", function(Value)
 print("Hello World!")
 end})
 
-local Tab1 = Window:MakeTab({"evade", "evade"})
+local Tab1 = Window:MakeTab({"AUTO FARM,AIMBOT,SPY", "AUTO FARM,AIMBOT,SPY"})
+
+Tab1:AddButton({"OP Infinite Stamina Auto Hunt Players Auto Farm", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OhhMyGehlee/sak/refs/heads/main/for"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"OP SCRIPT KILL AURA AUTO RAID AUTO DUNGEON AND MORE", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OhhMyGehlee/sh/refs/heads/main/a"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"OP Fast Kill Aura Auto Hatch Auto Roll Auto Farm", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OhhMyGehlee/sh/refs/heads/main/a"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"battleground", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hehehe9028/THE-strongest-battleground/refs/heads/main/HOKALAZA%20The%20strongest%20battleground"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"The Strongest Battlegrounds", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hehehe9028/THE-strongest-battleground/refs/heads/main/HOKALAZA%20The%20strongest%20battleground"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"Fish Key Lessa script", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KoonpeatchRE/Lonely_Hub/refs/heads/main/Fish_It"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"esp and hitbox expander", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EnesKam21/deadlineesp/refs/heads/main/deadlineesp.lua"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"rainbow script", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ArceusXArchivezx/Game/refs/heads/main/ArceusXArchive"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"knockout simulator", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Matej1912/Knockout-Simulator/refs/heads/main/Script"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"insta kill", function(Value)
+loadstring(game:HttpGet("https://noxhub.store/loader"))()
+print("Hello World!")
+end})
+
+local Tab1 = Window:MakeTab({"trollagem🤣", "trollagem🤣"})
+
+
+Tab1:AddButton({"music player gui", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/davidgames3d/Script-roblox/main/Music%20playey.lua"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"kater hub universal", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KATERGaming/Roblox/main/KaterHub.Lua"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"FE Trolling GUI", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yofriendfromschool1/Sky-Hub/main/FE%20Trolling%20GUI.luau"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"soluna troll brookhaven", function(Value)
+loadstring(game:HttpGet("https://soluna-script.vercel.app/brookhaven.lua",true))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"conquista fake doors", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/davidgames3d/Script-roblox/main/conquista%20falsa%20doors.lua"))()
+print("Hello World!")
+end}) 
+
