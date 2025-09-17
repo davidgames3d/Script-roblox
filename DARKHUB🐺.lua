8c6f7f581d600bf731c073bc5d4a5f3e0ea8772e
davidgames3d <pmartinsd9@gmail.com>
2025-09-17 13:15:27 -0300

    Atualizar o DARKHUB🐺.lua

--- DARKHUB🐺.lua
+++ DARKHUB🐺.lua
@@ -7,7 +7,7 @@ local Window = redzlib:MakeWindow({
 local Tab1 = Window:MakeTab({"main", "main"})
 
 Window:AddMinimizeButton({
-    Button = { Image = "rbxassetid://81494718138950", BackgroundTransparency = 0 },
+    Button = { Image = "rbxassetid://9265687294", BackgroundTransparency = 0 },
     Corner = { CornerRadius = UDim.new(35, 1) },
 })
 
@@ -30,7 +30,7 @@ Window:AddMinimizeButton({
 Tab1:AddDiscordInvite({
     Name = "davidgames3d server",
     Description = "Junte-se a nós",
-    Logo = "rbxassetid://8592618664",
+    Logo = "rbxassetid://81494718138950",
     Invite = "https://discord.gg/qXkvYQYKZG",
 })
 
