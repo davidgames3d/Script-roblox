7afe6661f81f49c2af379089640c1a3bd0a9db17
davidgames3d <pmartinsd9@gmail.com>
2025-09-17 15:56:19 -0300

    Criar R6R15.lua

---
+++ R6R15.lua
@@ -0,0 +1,116 @@
+local Tab1 = Window:MakeTab({"R6 and r15⬜", "R6 and r15⬜"})
+
+Tab1:AddButton({"hacker FE", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Legenda06/Client-Replication/main/Fe"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"FE MONSTER", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0Ben1/fe/main/obf_rTvXTs8F16D8D2oiLxZ62E1E9jT1we312yUyJr2h72Vwqr32l37rirU1S89hqRV7.lua.txt"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"change rig type", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Change-Rig-Type-29217"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"animate R6-R15 fe", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-LX63-FE-animate-R6-R15-1170"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"DcruzRace", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-DcruzRace-GUI-53539"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"fe laser r6", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Green-baseplate.-fe-laser-script-r6-53197"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"click fling", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-better-click-fling-universal-52247"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"fe ghost", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Ghost-51951"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"rscripter hub", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Rscripter-Hub-52549"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"rewind time", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Rewind-time-52957"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"combat r6", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Combat-Script-r6-Only-52184"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"r15 free bundles", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Free-Bundles-R15-3356"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"dog or cat R15", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Fe-dog-or-cat-R15-ONLY-53314"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"roblox egor", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-roblox-egor-script-53041"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"head remover", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Head-Remover-51769"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"vr7", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/tomtom's-Place-VR7-51692"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"snake avatar", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Snake-Avatar-51248"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"script fe r15", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-SCRIPT-FE-R15-51561"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"fe dog", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Green-baseplate.-Fe-dog-by-me-and-someone-51615"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"best hub 50 scripts", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Best-hub-50-scripts-plus-50250"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"remains hub v2", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-RemainsHub-V2-50805"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"hamsterball", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Remade-hamsterball-script-41893"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"zero gravity", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Zero-Gravity-28484"))()
+print("Hello World!")
+end})
