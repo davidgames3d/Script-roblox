8c264e6c81fd5f6b300d47ab93cea10ccd072701
davidgames3d <pmartinsd9@gmail.com>
2025-09-17 15:52:47 -0300

    Criar doors.lua

---
+++ doors.lua
@@ -0,0 +1,26 @@
+local Tab1 = Window:MakeTab({"doors🚪👁", "doors🚪👁"})
+
+Tab1:AddButton({"KITTY CATS DOORS", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CatEnddroid/Kitty-Cats-Doors-Beta/refs/heads/main/hub.lua"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"spawn tools", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Darli17/Entities/refs/heads/main/Tools"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"doors script", function(Value)
+loadstring(game:HttpGet(('https://pastefy.app/awczkDwJ/raw'),true))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"NullFire", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NuIlFire/NullFire/main/loader.lua"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"saturn hub", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JScripter-Lua/Saturn_Hub_Products/refs/heads/main/Saturn_Hub_Doors.lua"))() 
+print("Hello World!")
+end}) 
