7cc37cf0a56b494454cf0159eda04ebb8cdb5f45
davidgames3d <pmartinsd9@gmail.com>
2025-09-17 15:57:30 -0300

    Criar murdermystery.lua

---
+++ murdermystery.lua
@@ -0,0 +1,49 @@
+local Tab1 = Window:MakeTab({"murder Mystery 2🔪", "murder Mystery 2🔪"})
+
+Tab1:AddButton({"instawin and xray", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Murder-Mystery-2-KEYLESS-InstaWin-AutoFarm-Kill-all-XRay-nd-more-by-Moondiety-48644"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"hack murder Mystery", function(Value)
+loadstring(game:HttpGet("https://pastefy.app/XpdRjdYD/raw", true))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"echelovanta", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/echelonvanta/Scripts/refs/heads/main/Loader"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"radeon hub", function(Value)
+loadstring(game:HttpGet("https://pastebin.com/raw/v1LKQGxN", true))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"become hub", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BaconBossScript/BeeconHub/main/BeeconHub"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"combochronic vailt", function(Value)
+loadstring(game:HttpGet("https://pastebin.com/raw/gE1u62QJ", true))()
+
+--get key: https://discord.gg/4z4vjkRyTJ
+print("Hello World!")
+end})
+
+Tab1:AddButton({"blind hub", function(Value)
+loadstring(game:HttpGet("https://pastebin.com/raw/0zcsYxVu"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"solix hub", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/debunked69/Solixreworkkeysystem/refs/heads/main/solix%20new%20keyui.lua"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"foggy hub", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FOGOTY/foggy-loader/refs/heads/main/loader"))()
+print("Hello World!")
+end})
+
