b20d1b67db67f9de577589a57d0755b1b34f5403
davidgames3d <pmartinsd9@gmail.com>
2025-09-17 15:49:48 -0300

    Criar brainrot.lua

---
+++ brainrot.lua
@@ -0,0 +1,41 @@
+local Tab1 = Window:MakeTab({"roube um brainrot🧠🪐", "roube um brainrot🧠🪐"})
+
+Tab1:AddButton({"steal a brainrot script", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gumanba/Scripts/refs/heads/main/StealaBrainrot", true))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"op auto all", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OverflowBGSI/Overflow/refs/heads/main/loader.txt"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"the best hub", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KaspikScriptsRb/steal-a-brainrot/refs/heads/main/.lua"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"brainrot auto farm", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/m00ndiety/Steal-a-brainrot/refs/heads/main/Steal-a-Brainrot"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"ronix hub", function(Value)
+loadstring(game:HttpGet("pastebin.com/raw/HFx6faQY"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"rift hub", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Steal-a-Brainrot-b-51993"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"6 7 hub", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Steal-a-Brainrot-67-BEST-GUI-AUTO-53128"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"atherhub", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Steal-a-Brainrot-Atherhub-Steal-a-Brainrot-OP-50682"))()
+print("Hello World!")
+end})
