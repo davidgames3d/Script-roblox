72b3a7bcc5bfd3f1c9625a6da07a05dd916fee39
davidgames3d <pmartinsd9@gmail.com>
2025-09-17 16:00:13 -0300

    Criar deadrails.lua

---
+++ deadrails.lua
@@ -0,0 +1,30 @@
+local Tab1 = Window:MakeTab({"dead rails🧟‍♂️🚂", "dead rails🧟‍♂️🚂"})
+
+Tab1:AddButton({"nathub", function(Value) loadstring(game:HttpGet("https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/NatHub.lua"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"dead rails hack mod", function(Value)
+loadstring(game:HttpGet("https://pastebin.com/raw/9xvD1pig", true))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"auto ufo challenge", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yoursvexyyy/deadrails/refs/heads/main/autoufo"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"tn hub", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/thiennrb7/Script/refs/heads/main/Bringall"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"hoho Dead rails", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"moon hub", function(Value)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/m00ndiety/Moondiety/refs/heads/main/Loader'))()
+print("Hello World!")
+end})
