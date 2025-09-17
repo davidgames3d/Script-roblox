a545f75b4afc2f93f088202cdc6f480b10dc322f
davidgames3d <pmartinsd9@gmail.com>
2025-09-17 15:47:13 -0300

    Criar dandyworld.lua

---
+++ dandyworld.lua
@@ -0,0 +1,36 @@
+local Tab1 = Window:MakeTab({"dandy world🧸", "dandy world🧸"})
+
+Tab1:AddButton({"G0bbyD0llan", function(Value)
+loadstring(game:HttpGet("https://pastebin.com/raw/FBRnb7S7"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"soup hub", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Soup182/Soup-Hub-Dandy-s-World-Hub-/refs/heads/main/Dw%20Soup%20Hub.lua"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"aussie wire", function(Value)
+loadstring(game:HttpGet("https://pastebin.com/raw/9xvD1pig", true))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"abraço hub?", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BobJunior1/Sup/refs/heads/main/Bobhub"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"astral hub", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/StarlessScripts/Astral-Dandy-s-world/refs/heads/main/Starless"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"dandy World mobile", function(Value)
+loadstring(game:HttpGet("https://pastebin.com/raw/Y7uh3UZf"))();
+print("Hello World!")
+end})
+
+Tab1:AddButton({"dandy World gui", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gumanba/Scripts/main/Dandy"))()
+print("Hello World!")
+end})
