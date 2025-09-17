6bc77d09a914de3a076107a933ae32cdd06c351d
davidgames3d <pmartinsd9@gmail.com>
2025-09-17 15:54:46 -0300

    Criar naturaldisaster.lua

---
+++ naturaldisaster.lua
@@ -0,0 +1,66 @@
+local Tab1 = Window:MakeTab({"natural disastre🌋", "natural disastre🌋"})
+
+Tab1:AddButton({"xvc hub", function(Value)
+loadstring(game:HttpGet("https://pastebin.com/raw/Piw5bqGq"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"null Fire solara", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Fire-Hub-Solara-and-mobile-support-17673"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"nds hub", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Natural-Disaster-Survival-Katers-NDS-Hub-19533"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"jesus sobre a água", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Natural-Disaster-Survival-Choose-Map-NoFall-WaterWalk-7793"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"auto farm", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Natural-Disaster-Survival-Auto-Farm-5285"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"i was here", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Natural-Disaster-Survival-I-was-here-3355"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"comprehensive gui", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Comprehensive-GUI_182"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"free ballon giver", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Free-CLIENTSIDED-Balloon-Giver!_175"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"6 games", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Unique-Hub-v2-(6-Games)-2745"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"server breaker", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Natural-Disaster-Survival-natural-disaster-survival-server-breaker-53457"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"fe fling gui", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-FE-Flinger-GUI-WORKS-ANYWHERE-53326"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"blueRed hub", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-BlueRed-Hub-V21-48687"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"dollar hub", function(Value)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Dollar-Hub-47561"))()
+print("Hello World!")
+end})
