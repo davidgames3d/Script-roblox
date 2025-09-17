3abe5800c952e07029d4d7e5c5813526efc0a82f
davidgames3d <pmartinsd9@gmail.com>
2025-09-17 15:51:04 -0300

    Criar bloxfruit.lua

---
+++ bloxfruit.lua
@@ -0,0 +1,51 @@
+local Tab1 = Window:MakeTab({"blox fruit🍎", "blox fruit🍎"})
+
+Tab1:AddButton({"raito hub", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Efe0626/RaitoHub/main/Script"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"speed hub x", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"redz hub", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/Min/refs/heads/main/MinXt2Eng"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"banana hub", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nghia11n/Banana-Hub/main/bananahub.lua"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"w azure hub", function(Value)
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"min hub", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JonnyCheeser/bloxfruits/main/minhubv4"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"aurora", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jadelly261/BloxFruits/main/Aurora", true))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"hoho hub", function(Value)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/ascn123/HOHO_H/main/Loading_UI'))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"cokka hub", function(Value)
+loadstring(game:HttpGet"https://raw.githubusercontent.com/UserDevEthical/Loadstring/main/CokkaHub.lua")()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"annie hub", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Anniecuti/Free-Scr/main/Annie-Hub.lua"))()
+print("Hello World!")
+end})
