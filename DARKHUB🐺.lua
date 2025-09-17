25aab836f0a3c39fce25cdfa47bad3211f0f4d7f
davidgames3d <pmartinsd9@gmail.com>
2025-09-16 23:45:32 -0300

    Atualizar o DARKHUB🐺.lua

--- DARKHUB🐺.lua
+++ DARKHUB🐺.lua
@@ -516,6 +516,31 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Soup182/Soup-Hub-Dand
 print("Hello World!")
 end})
 
+Tab1:AddButton({"aussie wire", function(Value)
+loadstring(game:HttpGet("https://pastebin.com/raw/9xvD1pig", true))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"abraço hub?", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BobJunior1/Sup/refs/heads/main/Bobhub"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"astral hub", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/StarlessScripts/Astral-Dandy-s-world/refs/heads/main/Starless"))()
+print("Hello World!")
+end})
+
+Tab1:AddButton({"dandy World mobile", function(Value)
+loadstring(game:HttpGet("https://pastebin.com/raw/Y7uh3UZf"))();
+print("Hello World!")
+end})
+
+Tab1:AddButton({"dandy World gui", function(Value)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gumanba/Scripts/main/Dandy"))()
+print("Hello World!")
+end})
+
 local Tab1 = Window:MakeTab({"forsaken🏚", "forsaken🏚"})
 
 Tab1:AddButton({"forsaken spy", function(Value)
