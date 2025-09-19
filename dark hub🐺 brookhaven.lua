154e921f218e0885bedcff5bd2017b1022210748
davidgames3d <pmartinsd9@gmail.com>
2025-09-18 21:06:53 -0300

    Atualizar o dark hub🐺 brookhaven.lua

--- dark hub🐺 brookhaven.lua
+++ dark hub🐺 brookhaven.lua
@@ -6,3 +6,37 @@ local Window = redzlib:MakeWindow({
 })
 local Tab1 = Window:MakeTab({"main", "house"})
 
+Window:AddMinimizeButton({
+    Button = { Image = "rbxassetid://9265687294", BackgroundTransparency = 0 },
+    Corner = { CornerRadius = UDim.new(35, 1) },
+})
+
+Tab1:AddDiscordInvite({
+    Name = "davidgames3d server",
+    Description = "Junte-se a nós",
+    Logo = "rbxassetid://6604011398",
+    Invite = "https://discord.gg/qXkvYQYKZG",
+})
+
+local Paragraph = Tab1:AddParagraph({"aviso", "essa é a versão \para brookhaven do dark hub"})
+
+  local Dialog = Window:Dialog({
+    Title = "bem-vindo(a)",
+    Text = "essa é uma versão para brookhaven do dark hub criada por david",
+    Options = {
+      {"continuar", function()
+        
+      end},
+      {"obrigado", function()
+        
+      end},
+      {"Cancelar", function()
+        
+      end}
+    }
+  })
+
+Tab1:AddButton({"dark hub🐺 original", function(Value) 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/davidgames3d/Script-roblox/main/DARKHUB🐺.lua"))()
+print("Hello World!")
+end})
