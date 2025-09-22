16ea86637554645261f0e01f768b0074d25d5d32
davidgames3d <pmartinsd9@gmail.com>
2025-09-22 12:57:00 -0300

    Criar DARK HUB VIP⭐ version.lua

---
+++ DARK HUB VIP⭐ version.lua
@@ -0,0 +1,22 @@
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/Library-ui/refs/heads/main/Redzhubui"))()
+local Window = redzlib:MakeWindow({
+  Title = "DARK HUB⭐vip universal",
+  SubTitle = "by davidgames3d",
+  SaveFolder = "testando | redz lib v5.lua"
+})
+
+Window:AddMinimizeButton({
+    Button = { Image = "rbxassetid://99784233691859", BackgroundTransparency = 0 },
+    Corner = { CornerRadius = UDim.new(35, 1) },
+})Tab1:AddDiscordInvite({
+    Name = "DARK HUB SERVER",
+    Description = "Join server",
+    Logo = "rbxassetid://7096480483",
+    Invite = "https://discord.gg/qXkvYQYKZG",
+})
+
+local Tab1 = Window:MakeTab({"main", "house"})
+
+Tab1:AddButton({"Versão normal", function(Value) 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/davidgames3d/Script-roblox/main/DARKHUB🐺.lua"))()
+end})
